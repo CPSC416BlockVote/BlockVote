b0b3dd8cb8258cf05ad3826da36ff59314342241
UBC Student <myye@students.cs.ubc.ca>
2022-03-31 18:31:57 -0700

    Basic Transaction Implementation for M1 TODO:

--- blockchain/transaction.go
+++ blockchain/transaction.go
@@ -1,8 +1,203 @@
 package blockchain
 
+import (
+	"bytes"
+	"crypto/ecdsa"
+	"crypto/elliptic"
+	"crypto/rand"
+	"crypto/sha256"
+	wallet "cs.ubc.ca/cpsc416/BlockVote/Identity"
+	"encoding/gob"
+	"encoding/hex"
+	"log"
+	"math/big"
+	"time"
+)
+
+//type Transaction struct {
+//	Data      *Ballot
+//	TXID      []byte
+//	Signature []byte
+//	PublicKey []byte
+//}
+
 type Transaction struct {
-	Data      *Ballot
-	TXID      []byte
-	Signature []byte
-	PublicKey []byte
+	Data    time.Time
+	ID      []byte
+	Inputs  []TxInput
+	Outputs []TxOutput
+}
+
+// ----- Transaction APIs -----
+
+func (tx *Transaction) Hash() []byte {
+	var hash [32]byte
+
+	txCopy := *tx
+	txCopy.ID = []byte{}
+
+	hash = sha256.Sum256(txCopy.Serialize())
+
+	return hash[:]
+}
+
+func (tx Transaction) Serialize() []byte {
+	var encoded bytes.Buffer
+
+	enc := gob.NewEncoder(&encoded)
+	err := enc.Encode(tx)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	return encoded.Bytes()
+}
+
+func (tx *Transaction) SetID() {
+	var encoded bytes.Buffer
+	var hash [32]byte
+
+	encode := gob.NewEncoder(&encoded)
+	err := encode.Encode(tx)
+
+	if err != nil {
+		log.Panic(err)
+	}
+
+	hash = sha256.Sum256(encoded.Bytes())
+	tx.ID = hash[:]
+}
+
+func NewTransaction(voterName, candidateName, id, from, to string, amount int, chain *BlockChain) *Transaction {
+	var inputs []TxInput
+	var outputs []TxOutput
+
+	wallets, err := wallet.CreateVoter(voterName, id)
+
+	if err != nil {
+		log.Panic(err)
+	}
+
+	w := wallets.GetWallet(from)
+	pubKeyHash := wallet.PublicKeyHash(w.PublicKey)
+
+	// TODO: need to find spendable number of block
+	acc, validOutputs := chain.FindSpendableOutputs(pubKeyHash, amount)
+
+	// TODO: change acc later, connect with candidateName with to
+	if acc < amount {
+		log.Panic("Error: not enough funds")
+	}
+
+	for txid, outs := range validOutputs {
+		txID, err := hex.DecodeString(txid)
+
+		if err != nil {
+			log.Panic(err)
+		}
+
+		for _, out := range outs {
+			input := TxInput{txID, out, nil, w.PublicKey}
+			inputs = append(inputs, input)
+		}
+	}
+
+	// TODO: candidate mines his output
+	outputs = append(outputs, *NewTXOutput(amount, to))
+
+	if acc > amount {
+		outputs = append(outputs, *NewTXOutput(acc-amount, from))
+	}
+
+	tx := Transaction{time.Now(), nil, inputs, outputs}
+	tx.ID = tx.Hash()
+
+	chain.SignTransaction(&tx, w.PrivateKey)
+
+	return &tx
+}
+
+func (tx *Transaction) Sign(privKey ecdsa.PrivateKey, prevTXs map[string]Transaction) {
+
+	for _, in := range tx.Inputs {
+		if prevTXs[hex.EncodeToString(in.ID)].ID == nil {
+			log.Panic("ERROR: Previous transaction is not correct")
+		}
+	}
+
+	txCopy := tx.TrimmedCopy()
+
+	for inId, in := range txCopy.Inputs {
+		prevTX := prevTXs[hex.EncodeToString(in.ID)]
+		txCopy.Inputs[inId].Signature = nil
+		txCopy.Inputs[inId].PubKey = prevTX.Outputs[in.Out].PubKeyHash
+		txCopy.ID = txCopy.Hash()
+		txCopy.Inputs[inId].PubKey = nil
+
+		r, s, err := ecdsa.Sign(rand.Reader, &privKey, txCopy.ID)
+		if err != nil {
+			log.Panic(err)
+		}
+		signature := append(r.Bytes(), s.Bytes()...)
+
+		tx.Inputs[inId].Signature = signature
+
+	}
+}
+
+func (tx *Transaction) Verify(prevTXs map[string]Transaction) bool {
+
+	for _, in := range tx.Inputs {
+		if prevTXs[hex.EncodeToString(in.ID)].ID == nil {
+			log.Panic("Previous transaction not correct")
+		}
+	}
+
+	txCopy := tx.TrimmedCopy()
+	curve := elliptic.P256()
+
+	for inId, in := range tx.Inputs {
+		prevTx := prevTXs[hex.EncodeToString(in.ID)]
+		txCopy.Inputs[inId].Signature = nil
+		txCopy.Inputs[inId].PubKey = prevTx.Outputs[in.Out].PubKeyHash
+		txCopy.ID = txCopy.Hash()
+		txCopy.Inputs[inId].PubKey = nil
+
+		r := big.Int{}
+		s := big.Int{}
+
+		sigLen := len(in.Signature)
+		r.SetBytes(in.Signature[:(sigLen / 2)])
+		s.SetBytes(in.Signature[(sigLen / 2):])
+
+		x := big.Int{}
+		y := big.Int{}
+		keyLen := len(in.PubKey)
+		x.SetBytes(in.PubKey[:(keyLen / 2)])
+		y.SetBytes(in.PubKey[(keyLen / 2):])
+
+		rawPubKey := ecdsa.PublicKey{curve, &x, &y}
+		if ecdsa.Verify(&rawPubKey, txCopy.ID, &r, &s) == false {
+			return false
+		}
+	}
+
+	return true
+}
+
+func (tx *Transaction) TrimmedCopy() Transaction {
+	var inputs []TxInput
+	var outputs []TxOutput
+
+	for _, in := range tx.Inputs {
+		inputs = append(inputs, TxInput{in.ID, in.Out, nil, nil})
+	}
+
+	for _, out := range tx.Outputs {
+		outputs = append(outputs, TxOutput{out.Value, out.PubKeyHash})
+	}
+
+	txCopy := Transaction{time.Now(), tx.ID, inputs, outputs}
+
+	return txCopy
 }
--- go.mod
+++ go.mod
@@ -6,5 +6,5 @@ require (
 	github.com/DistributedClocks/tracing v0.0.0-20220202233639-0154e31ea72b
 	github.com/dgraph-io/badger/v3 v3.2103.2
 	github.com/mr-tron/base58 v1.2.0
-	golang.org/x/crypto v0.0.0-20200622213623-75b288015ac9
+	golang.org/x/crypto v0.0.0-20220331220935-ae2d96664a29
 )
