625e8dba73e5a1b4c3c81d5ce73b90c7858a4e09
YveTobio <1192289065@qq.com>
2022-04-04 22:31:52 -0700

    finished miner-client api and miner-coord register

--- blockvote/miner.go
+++ blockvote/miner.go
@@ -9,6 +9,7 @@ import (
 	"github.com/DistributedClocks/tracing"
 	"log"
 	"strings"
+	"sync"
 )
 
 type MinerConfig struct {
@@ -43,6 +44,13 @@ type GetBlockReply struct {
 	block blockchain.Block
 }
 
+type GetTxnPoolArgs struct {
+}
+
+type GetTxnPoolReply struct {
+	PeerTxnPool TxnPool
+}
+
 type SubmitTxnArgs struct {
 	Txn blockchain.Transaction
 }
@@ -55,25 +63,31 @@ type Miner struct {
 	Storage    *util.Database
 	Blockchain *blockchain.BlockChain
 
-	Info MinerInfo
+	Info         MinerInfo
+	ReceivedTxns map[string]bool
+	Candidates   []Identity.Wallets
+	MemoryPool   TxnPool
 
-	Candidates []Identity.Wallets
-	MemoryPool TxnPool
+	mu    sync.Mutex
+	cond  *sync.Cond
+	start bool
 }
 
 func NewMiner() *Miner {
 	return &Miner{
-		Storage: &util.Database{},
+		Storage:      &util.Database{},
+		ReceivedTxns: make(map[string]bool),
 	}
 }
 
 type TxnPool struct {
+	PendingTxns []blockchain.Transaction
 }
 
 func (m *Miner) Start(minerId string, coordAddr string, minerAddr string, difficulty uint8, mtrace *tracing.Tracer) error {
 	err := m.Storage.New("", true)
 	if err != nil {
-		util.CheckErr(err, "error when creating databse")
+		util.CheckErr(err, "error when creating database")
 	}
 	defer m.Storage.Close()
 
@@ -84,32 +98,11 @@ func (m *Miner) Start(minerId string, coordAddr string, minerAddr string, diffic
 		util.CheckErr(err, "error when initializing blockchain")
 	}
 
-	// miner can do proof-of-work
-	prevHash := m.Blockchain.LastHash
-	for i := 0; i < 10; i++ {
-		fmt.Printf("Block #%d:\n", i+1)
-		block := blockchain.Block{
-			PrevHash: prevHash,
-			BlockNum: uint8(i + 1),
-			Nonce:    0,
-			Txns:     []*blockchain.Transaction{},
-			MinerID:  minerId,
-			Hash:     []byte{},
-		}
-		pow := blockchain.NewProof(&block)
-		nonce, hash := pow.Run()
-		block.Nonce = nonce
-		block.Hash = hash
-		prevHash = hash
-
-		fmt.Printf("Nonce: %d\n", block.Nonce)
-		fmt.Printf("Hash: %x\n", block.Hash)
-		fmt.Println()
-	}
-
+	m.cond = sync.NewCond(&m.mu)
+	m.mu.Lock()
 	// starting API services
 	minerIP := minerAddr[0:strings.Index(minerAddr, ":")]
-	// >> coord
+	// << coord
 	minerAPICoord := new(MinerAPICoord)
 	minerAPICoord.m = m
 	coordListenAddr, err := util.NewRPCServerWithIp(minerAPICoord, minerIP)
@@ -119,7 +112,7 @@ func (m *Miner) Start(minerId string, coordAddr string, minerAddr string, diffic
 	m.Info.CoordListenAddr = coordListenAddr
 	log.Println("[INFO] Listen to coord's API requests at", m.Info.CoordListenAddr)
 
-	// >> client
+	// << client
 	minerAPIClient := new(MinerAPIClient)
 	minerAPIClient.m = m
 	clientListenAddr, err := util.NewRPCServerWithIp(minerAPIClient, minerIP)
@@ -129,7 +122,7 @@ func (m *Miner) Start(minerId string, coordAddr string, minerAddr string, diffic
 	m.Info.ClientListenAddr = clientListenAddr
 	log.Println("[INFO] Listen to clients' API requests at", m.Info.ClientListenAddr)
 
-	// >> miner
+	// << miner
 	minerAPIMiner := new(MinerAPIMiner)
 	minerAPIMiner.m = m
 	minerMinerAddr, err := util.NewRPCServerWithIp(minerAPIMiner, minerIP)
@@ -139,7 +132,63 @@ func (m *Miner) Start(minerId string, coordAddr string, minerAddr string, diffic
 	m.Info.MinerMinerAddr = minerMinerAddr
 	log.Println("[INFO] Listen to miners' API requests at", m.Info.MinerMinerAddr)
 
-	return errors.New("not implemented")
+	// started API clients
+	// >> coord
+	coordClient, err := util.NewRPCClient(minerAddr, coordAddr)
+	if err != nil {
+		return errors.New("cannot create client for coord")
+	}
+	minerInfo := MinerInfo{clientListenAddr, minerMinerAddr, clientListenAddr}
+	reply := RegisterReply{}
+	coordClient.Call("CoordAPIMiner.Register", RegisterArgs{minerInfo}, &reply)
+	// ASSUME: the returned peer list cannot be empty nor contain the miner itself
+	// TODO: loop throught the peer list of just choose one?
+	if len(reply.PeerAddrList) > 0 {
+		toPullMinerAddr := reply.PeerAddrList[0]
+		// get txn pool from the peer
+		// >> miner
+		minerClient, err := util.NewRPCClient(minerAddr, toPullMinerAddr)
+		if err != nil {
+			return errors.New("cannot create client for miner")
+		}
+		reply := GetTxnPoolReply{}
+		minerClient.Call("MinerAPIMiner.GetTxnPool", GetTxnPoolArgs{}, &reply)
+		m.MemoryPool = reply.PeerTxnPool
+		m.start = true
+		m.cond.Broadcast()
+	} else {
+		return errors.New("empty peer list. This should never happens")
+	}
+	m.mu.Unlock()
+
+	// miner does proof-of-work forever
+	i := 0
+	for {
+		prevHash := m.Blockchain.LastHash
+		if len(m.MemoryPool.PendingTxns) > 0 {
+			// TODO: implement the select
+			fmt.Printf("Block #%d:\n", i+1)
+			block := blockchain.Block{
+				PrevHash: prevHash,
+				BlockNum: uint8(i + 1),
+				Nonce:    0,
+				Txns:     []*blockchain.Transaction{},
+				MinerID:  minerId,
+				Hash:     []byte{},
+			}
+			pow := blockchain.NewProof(&block)
+			nonce, hash := pow.Run()
+			block.Nonce = nonce
+			block.Hash = hash
+			prevHash = hash
+
+			fmt.Printf("Nonce: %d\n", block.Nonce)
+			fmt.Printf("Hash: %x\n", block.Hash)
+			fmt.Println()
+			i++
+		}
+	}
+	return nil
 }
 
 // ----- APIs for coord -----
@@ -162,6 +211,11 @@ func (api *MinerAPIMiner) GetBlock(args GetBlockArgs, reply *GetBlockReply) erro
 	return nil
 }
 
+func (api *MinerAPIMiner) GetTxnPool(args GetTxnPoolArgs, reply *GetTxnPoolReply) error {
+	reply.PeerTxnPool = api.m.MemoryPool
+	return nil
+}
+
 // ----- APIs for client
 
 type MinerAPIClient struct {
@@ -169,5 +223,17 @@ type MinerAPIClient struct {
 }
 
 func (api *MinerAPIClient) SubmitTxn(args SubmitTxnArgs, reply *SubmitTxnReply) error {
+	api.m.mu.Lock()
+	for !api.m.start {
+		api.m.cond.Wait()
+	}
+	defer api.m.mu.Unlock()
+	if api.m.Blockchain.ValidateTxn(&args.Txn) {
+		sid := string(args.Txn.ID)
+		if !api.m.ReceivedTxns[sid] {
+			api.m.ReceivedTxns[sid] = true
+			api.m.MemoryPool.PendingTxns = append(api.m.MemoryPool.PendingTxns, args.Txn)
+		}
+	}
 	return nil
 }
