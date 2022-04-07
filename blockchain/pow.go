91dfaec8da741541327ee628833dc12e3a154deb
zyt <helloworld98zyt@gmail.com>
2022-04-06 23:33:27 -0700

    small fix

--- blockchain/pow.go
+++ blockchain/pow.go
@@ -21,7 +21,7 @@ const NumZeros = 8
 // NewProof creates a new ProofOfWork structure
 func NewProof(b *Block) *ProofOfWork {
 	target := big.NewInt(1)
-	target.Lsh(target, uint(255-NumZeros))
+	target.Lsh(target, uint(256-NumZeros))
 	//fmt.Printf("Target: %x\n", target)
 	pow := &ProofOfWork{b, target}
 	return pow
